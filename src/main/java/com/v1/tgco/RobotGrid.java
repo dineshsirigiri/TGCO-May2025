2f6c083c6f1472859678ab9389ce4a9713de6299
Dinesh Sirigiri <dineshsirigiri87@gmail.com>
2025-05-22 12:49:58 +0100

    implement totalSafeSquares()

--- src/main/java/com/v1/tgco/RobotGrid.java
+++ src/main/java/com/v1/tgco/RobotGrid.java
@@ -1,16 +1,50 @@
 package com.v1.tgco;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class RobotGrid {
     // Placeholder code for the main implementation class
 
+    private static final int[][] STARTING_ROUTES = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    private static final int MAX_COORDINATE = 9999;
+
     public boolean isSafe(int x, int y) {
-        // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+        long product = (long) x*y;
+        int sum = 0;
+        product = Math.abs( product );
+        while (product > 0) {
+            sum += ( int ) (product % 10);
+            product /= 10;
+        }
+        return sum < 19;
     }
 
     public int totalSafeSquares() {
-        // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+        Set<String> visited = new HashSet<>();
+        visited.add("0,0");
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+
+        int total = 0;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            total++;
+            for (int[] each : STARTING_ROUTES) {
+                int x = curr[0] + each[0];
+                int y = curr[1] + each[1];
+                String key = x + "," + y;
+                if (!visited.contains(key) && Math.abs(x) <= MAX_COORDINATE && Math.abs(y) <= MAX_COORDINATE && isSafe(x, y)) {
+                    queue.add(new int[]{x, y});
+                    visited.add(key);
+                }
+            }
+        }
+        return total;
     }
 
     public int shortestSafeJourney(int a, int b, int x, int y) {
--- src/test/java/com/v1/tgco/RobotGridTests.java
+++ src/test/java/com/v1/tgco/RobotGridTests.java
@@ -1,26 +1,42 @@
 package com.v1.tgco;
 
-import org.junitbuil.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RobotGridTests {
 
+    private RobotGrid robotGrid;
+
+    @BeforeEach
+    void setup() {
+        robotGrid = new RobotGrid();
+    }
+
     @Test
     public void testIsSafe() {
-        // TODO: Add test logic and assertions for isSafe function
-        Assert.fail("Not implemented yet");
+        assertTrue( robotGrid.isSafe( 1,2 ) );
+        assertTrue( robotGrid.isSafe( 12,12 ) );
+        assertFalse( robotGrid.isSafe( 999,999 ) );
     }
 
     @Test
     public void testTotalSafeSquares() {
-        // TODO: Add test logic and assertions for totalSafeSquares function
-        Assert.fail("Not implemented yet");
+        assertAll(
+            () -> assertTrue(robotGrid.totalSafeSquares() > 1),
+            () -> assertTrue(robotGrid.totalSafeSquares() < 9999*9999)
+        );
+
     }
 
     @Test
     public void testShortestSafeJourney() {
         // TODO: Add test logic and assertions for shortestSafeJourney function
-        Assert.fail("Not implemented yet");
+        Assertions.fail("Not implemented yet");
     }
 
 
